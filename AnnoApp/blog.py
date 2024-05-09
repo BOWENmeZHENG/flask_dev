60080344731c892e4f6e06828bd607e0c1b269b3
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2024-05-09 22:15:30 +0000

    first

---
+++ AnnoApp/blog.py
@@ -0,0 +1,95 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+from AnnoApp.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created'
+        ' FROM post p'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        if not title:
+            flash('Title is required.')
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body)'
+                ' VALUES (?, ?)',
+                (title, body)
+            )
+            db.commit()
+            flash(f"Record {title} has been saved!")
+            posts = db.execute(
+                'SELECT p.id, title, body, created'
+                ' FROM post p'
+                ' ORDER BY created DESC'
+            ).fetchall()
+            return render_template('blog/index.html', posts=posts)
+
+    return render_template('blog/create.html')
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created'
+        ' FROM post p'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    return post
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+def update(id):
+    post = get_post(id)
+    db = get_db()
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        if not title:
+            flash('Title is required.')
+        else:           
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            flash(f"Record {title} has been updated!")
+            posts = db.execute(
+                'SELECT p.id, title, body, created'
+                ' FROM post p'
+                ' ORDER BY created DESC'
+            ).fetchall()
+            return render_template('blog/index.html', posts=posts)
+
+    return render_template('blog/update.html', post=post)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    flash("Record has been deleted!")
+    posts = db.execute(
+        'SELECT p.id, title, body, created'
+        ' FROM post p'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
